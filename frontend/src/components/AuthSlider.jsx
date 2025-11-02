1ab8681e160bc65b7c305a73c3edc6da13f1736d
Jack17kdb <jack@kali.Johnson.com>
2025-11-02 12:51:25 +0300

    login design

---
+++ frontend/src/components/AuthSlider.jsx
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const AuthSlider = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [pictures, setPictures] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [fade, setFade] = useState(true);
+  const [ready, setReady] = useState(false);
+
+  useEffect(() => {
+    loadPhotos();
+  }, []);
+
+  useEffect(() => {
+    if (pictures.length === 0 || !ready) return;
+    const interval = setInterval(() => {
+      setFade(false);
+      setTimeout(() => {
+        setCurrentIndex((prev) =>
+          prev === pictures.length - 1 ? 0 : prev + 1
+        );
+        setFade(true);
+      }, 500);
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, [pictures, ready]);
+
+  const loadPhotos = async () => {
+    try {
+      setLoading(true);
+      const url = `https://api.spoonacular.com/recipes/random?apiKey=${
+        import.meta.env.VITE_FOOD_API
+      }&number=3`;
+      const response = await axios.get(url);
+      const images = response.data.recipes.map((recipe) => recipe.image);
+      setPictures(images);
+      setCurrentIndex(0);
+      setReady(true);
+    } catch (error) {
+      console.log("Error fetching images", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="relative h-full w-full flex flex-col justify-center items-center rounded-xl">
+      {loading ? (
+        <div className="h-full w-full animate-pulse rounded-xl bg-gray-300"></div>
+      ) : (
+        <img
+          src={pictures[currentIndex]}
+          alt="Food slide"
+          className={`w-full h-full rounded-xl transition-opacity duration-500 ${
+            fade ? "opacity-100" : "opacity-0"
+          }`}
+        />
+      )}
+      <div className="absolute flex bottom-3 gap-2">
+        {pictures?.map((_, index) => (
+          <div
+            key={index}
+            onClick={() => setCurrentIndex(index)}
+            className={`transition-all duration-300 rounded-full cursor-pointer ${
+              index === currentIndex
+                ? "bg-orange-500 w-6 h-2"
+                : "bg-gray-400 w-2 h-2"
+            }`}
+          ></div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default AuthSlider;
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,116 @@
+import React, { useState } from "react";
+import { FaUser, FaLock, FaEye, FaEyeSlash } from "react-icons/fa";
+import { FaGoogle, FaFacebookF } from "react-icons/fa";
+import AuthSlider from "../components/AuthSlider.jsx";
+
+const Login = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePassword = () => {
+    setShowPassword((prev) => !prev);
+  };
+
+  return (
+    <div className="min-h-screen py-5 bg-gray-300">
+      <div className="grid grid-cols-1 md:grid-cols-2 items-center p-6 m-8 gap-4 bg-white rounded-2xl py-10">
+        <div className="flex flex-col items-center justify-center">
+          <div className="text-center mb-8">
+            <h1 className="font-bold text-4xl mb-2">Welcome Back!</h1>
+            <p className="text-sm text-gray-500">
+              Sign in with your username and password
+            </p>
+          </div>
+          <div className="flex flex-col gap-3">
+            <div className="flex items-center border border-gray-300 focus-within:ring-2 focus-within:ring-blue-400 focus-within:outline-none rounded-full p-3 h-10 w-75">
+              <FaUser className="text-gray-300 mr-3" />
+              <input
+                type="text"
+                placeholder="Username"
+                className="w-full outline-none text-gray-700"
+              />
+            </div>
+
+            <div className="flex items-center border border-gray-300 focus-within:ring-2 focus-within:ring-blue-400 focus-within:outline-none rounded-full p-3 h-10 w-75">
+              <FaLock className="text-gray-300 mr-3" />
+              <input
+                type={showPassword ? "text" : "password"}
+                placeholder="Password"
+                className="w-full outline-none text-gray-700"
+              />
+              {!showPassword ? (
+                <FaEye
+                  size={20}
+                  onClick={togglePassword}
+                  className="flex-end text-gray-300 hover:text-blue-400 cursor-pointer"
+                />
+              ) : (
+                <FaEyeSlash
+                  size={20}
+                  onClick={togglePassword}
+                  className="flex-end text-gray-300 hover:text-blue-400 cursor-pointer"
+                />
+              )}
+            </div>
+            <div className="flex items-center justify-end">
+              <a
+                href="/forget-password"
+                className="font-semibold text-gray-800 hover:underline hover:text-blue-400 transition-all duration-300 text-sm"
+              >
+                Forget Password?
+              </a>
+            </div>
+            <div className="flex items-center justify-center">
+              <button
+                type="button"
+                className="bg-black text-white font-semibold cursor-pointer rounded-full p-2 h-10 active:scale-95 transition w-full"
+              >
+                Login
+              </button>
+            </div>
+
+            <div className="flex items-center my-4">
+              <div className="flex-grow border-t border-gray-400"></div>
+              <div className="mx-3 text-md text-black">or login with</div>
+              <div className="flex-grow border-t border-gray-400"></div>
+            </div>
+
+            <div className="flex items-center justify-center">
+              <button
+                type="button"
+                className="bg-transparent text-black border border-gray-300 text-sm cursor-pointer rounded-full p-2 h-10 active:scale-95 transition w-full flex items-center justify-center gap-2"
+              >
+                <FaGoogle className="text-[#DB4437] text-xl" />
+                <span className="font-medium text-sm">Login with Google</span>
+              </button>
+            </div>
+
+            <div className="flex items-center justify-center">
+              <button
+                type="button"
+                className="bg-transparent text-black border border-gray-300 text-sm cursor-pointer rounded-full p-2 h-10 active:scale-95 transition w-full flex items-center justify-center gap-2"
+              >
+                <FaFacebookF className="text-blue-400 text-xl" />
+                <span className="font-medium text-sm">Login with Facebook</span>
+              </button>
+            </div>
+
+            <div className="flex items-center justify-center gap-1 mt-10">
+              <span className="text-gray-700 text-sm">
+                Don't have an account?
+              </span>{" "}
+              <a
+                href="/register"
+                className="font-semibold text-sm text-gray-800 hover:underline hover:text-blue-400 transition-all duration-300"
+              >
+                Register Now
+              </a>
+            </div>
+          </div>
+        </div>
+        <AuthSlider className="hidden md:block" />
+      </div>
+    </div>
+  );
+};
+
+export default Login;
